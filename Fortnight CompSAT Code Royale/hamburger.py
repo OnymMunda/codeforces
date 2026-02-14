c2a2a59db836f9c888eb183f653fce818bc15d16
OnymMunda <jrw.munda@gmail.com>
2026-02-14 12:22:43 +0800

    finished fortnight 1

--- Fortnight CompSAT Code Royale/hamburger.py
+++ Fortnight CompSAT Code Royale/hamburger.py
@@ -3,43 +3,67 @@ n_b, n_s, n_c = map(int, input().split())
 p_b, p_s, p_c = map(int, input().split())
 rubles = int(input())
 
-max = 0
-recipe = {
-    "B": 0,
-    "S": 0,
-    "C": 0
-}
+max_burgers = 0
 
-for i in range(len(string)):
-    if string[i] == "B":
-        recipe["B"] += 1
-    if string[i] == "S":
-        recipe["S"] += 1
-    if string[i] == "C":
-        recipe["C"] += 1
+recipe = {
+    "B": 0, 
+    "S": 0, 
+    "C": 0
+    }
+
+for ch in string:
+    recipe[ch] += 1
+
 
 available = {
-    "B": 0,
-    "S": 0,
-    "C": 0
-}
+    "B": n_b, 
+    "S": n_s, 
+    "C": n_c
+    }
 
-available["B"] = n_b
-available["S"] = n_s
-available["C"] = n_c
+burger_cost = recipe["B"] * p_b + recipe["S"] * p_s + recipe["C"] * p_c
+while True:
 
-while available["B"] >= recipe["B"] and available["S"] >= recipe["S"] and available["C"] >= recipe["C"] :
+    if available["B"] >= recipe["B"]:
+        need_B = 0
+    else:
+        need_B = recipe["B"] - available["B"]
+
+    if available["S"] >= recipe["S"]:
+        need_S = 0
+    else:
+        need_S = recipe["S"] - available["S"]
+
+    if available["C"] >= recipe["C"]:
+        need_C = 0
+    else:
+        need_C = recipe["C"] - available["C"]
+   
+    cost = need_B * p_b + need_S * p_s + need_C * p_c
+
+    if cost > rubles:
+        break
+
+    rubles -= cost
+    available["B"] += need_B
+    available["S"] += need_S
+    available["C"] += need_C
+
     available["B"] -= recipe["B"]
     available["S"] -= recipe["S"]
     available["C"] -= recipe["C"]
-    max += 1
 
+    max_burgers += 1
 
-if available["B"] < recipe["B"]:
-    diff_B = recipe["B"] - available["B"]
-    price_B = diff_B * p_b
-    if price_B <= rubles:
-        rubles -= price_B
+    if recipe["B"] == 0:
+        available["B"] *= 0
+    if recipe["S"] == 0:
+        available["S"] *= 0
+    if recipe["C"] == 0:
+        available["C"] *= 0
 
-
-
+    if available["B"] == 0 and available["S"] == 0 and available["C"] == 0:
+        max_burgers += rubles // burger_cost
+        break
+    
+print(max_burgers)
